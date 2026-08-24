2449041b67e7593d36015eb21edea200c741e8f1
Sakshi04bari <sakshibari04@gmail.com>
2026-08-24 17:45:51 +0530

    Integrate trained model with FastAPI

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+import joblib
+from fastapi import FastAPI
+from sklearn.datasets import load_iris
+
+model = None
+iris = load_iris()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global model
+
+    model = joblib.load("ml/saved_model/model.joblib")
+    print("Model loaded successfully.")
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
@@ -10,4 +28,24 @@ def root():
 
 @app.post("/predict")
 def predict():
-    return {"prediction": "hardcoded_result"}
+    features = {
+    "sepal_length": 6.0,
+    "sepal_width": 2.9,
+    "petal_length": 4.5,
+    "petal_width": 1.5
+}
+
+    sample = [[
+        features["sepal_length"],
+        features["sepal_width"],
+        features["petal_length"],
+        features["petal_width"]
+    ]]
+
+    prediction = model.predict(sample)[0]
+    flower_name = iris.target_names[prediction]
+
+    return {
+        "prediction": int(prediction),
+        "flower": flower_name
+    }
